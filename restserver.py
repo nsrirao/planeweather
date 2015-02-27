0ff8860921a984534673e3fc109aaf7a74726b76
nsrirao <nsrirao@gmail.com>
2015-02-27 13:09:24 -0500

    View is working. Ajax call returning json data is not working. Using a static file for now.

--- restserver.py
+++ restserver.py
@@ -14,70 +14,95 @@ class flightdata:
         p = dict(p)
         print web.ctx.fullpath
         ## Form data from the GET URL
-        sourceAP = p['sourceAP']
-        destAP = p['destAP']
-        speed = float(p['speed'])
-        departure = p['departure']
-        interval = float(p['interval'])
+        sourceAP = p["sourceAP"]
+        destAP = p["destAP"]
+        speed = float(p["speed"])
+        departure = p["departure"]
+        interval = float(p["interval"])
         
         if sourceAP == None:
-            sourceAP = 'RDU'
+            sourceAP = "RDU"
         
         if destAP == None:
-            destAP = 'ATL'
+            destAP = "ATL"
             
         if departure == None:
             departure = datetime.now()
         else:
-            departure = None ###### !!!!!!!!!!!!!!!!!!!!!!!!!
+            print departure
         cachekey = (sourceAP, destAP, departure, interval)
         
         if cachekey in flightpaths:
-            return flightpaths[cachekey]
+            print "Cache Hit!"
+            return json.dumps(flightpaths[cachekey], indent=2).replace('\'','"')
         
-        point1 = getAirportLatLon(sourceAP)
+        point1 = getAirportLatLon(sourceAP)   
         point2 = getAirportLatLon(destAP)
         dis = geography.distance(point1,point2)
         b = geography.bearing(point1, point2)
  
         numIntervals = int((dis*1.0/speed) / interval)
-                
-        weatherdata = {}
-        weatherdata[point1] = getWeather( point1[0], point1[1] )
-        weatherdata[point2] = getWeather( point2[0], point2[1] )
+
+        generaldata = {
+        "origin": point1,
+        "dest": point2,
+        "distance": dis,
+        "bearing": b,
+        "numintervals": numIntervals,
+        "speed": speed,
+        }
+        weatherdata = []
+        weatherdata.append({"gen": generaldata})
+        wdict = getWeather( point1[0], point1[1] )
+        wdict["point"] = point1
+        weatherdata.append(wdict); 
+     
+        wdict = getWeather( point2[0], point2[1] )
+        wdict["point"] = point2
+        weatherdata.append(wdict); 
 
         for x in range(1,numIntervals):
             distancefrompoint1 = speed * x * interval
             pointX = geography.location(point1, distancefrompoint1, b) 
-            weatherdata[pointX] = getWeather(pointX[0], pointX[1])
+            wdict = getWeather(pointX[0], pointX[1])
+            wdict["point"] = pointX
+            weatherdata.append(wdict); 
+
+        
+        #The data is cached here
         flightpaths[cachekey] = weatherdata
-        return flightpaths[cachekey]
+        
+        with open('flightdata.json', 'w+') as fp:
+            json.dump(flightpaths[cachekey], fp, indent=2)
+        return json.dumps(flightpaths[cachekey], indent=2).replace('\'','"')
         
         
-def getAirportLatLon(apFAA='RDU'):
-    '''Look up a database of airport codes.
-    return a tuple(latitude,longitude)
-    '''
+def getAirportLatLon(apFAA="RDU"):
+    """Look up a database of IATA/FAA airport codes.
+    return a tuple(latitude,longitude) of that airport
+    """
     for record in airports:
-        if record['IATAFAA'] == apFAA:
-            return (float(record['Latitude']), float(record['Longitude']))
+        if record["IATAFAA"] == apFAA:
+            return (float(record["Latitude"]), float(record["Longitude"]))
            
 def getAirports():
-        '''Takes the csv file of airports data and converts to a dictionary.
-        '''
-        with open('static/airports.dat', 'r+') as csvfile:
-            fieldnames = ('AirportID',
-                            'Name',
-                            'City',
-                            'Country',
-                            'IATAFAA',
-                            'ICAO',
-                            'Latitude',
-                            'Longitude',
-                            'Altitude',
-                            'Timezone',
-                            'DST',
-                            'Tz',)
+        """Takes the csv file of airports data and converts to a dictionary.
+        This is invoked once during start so the data is in memory 
+        for faster access
+        """
+        with open("static/airports.dat", "r+") as csvfile:
+            fieldnames = ("AirportID",
+                            "Name",
+                            "City",
+                            "Country",
+                            "IATAFAA",
+                            "ICAO",
+                            "Latitude",
+                            "Longitude",
+                            "Altitude",
+                            "Timezone",
+                            "DST",
+                            "Tz",)
             reader = csv.DictReader(csvfile, fieldnames=fieldnames)
             a = []
             for record in reader:
@@ -90,11 +115,15 @@ if __name__ == "__main__":
     flightpaths = {}
     ## Read in the airports data
     airports = getAirports()
+    
+    #The URLs for accessing the REST services. Since the versioning info 
+    #is in the URL, the associated class can be different for other versions
     urls = (
-        '/v1/flightdata', 'flightdata',
-        '/v1/flightdata/', 'flightdata',
-        '/v1/flightdata/(.*)', 'flightdata',
+        "/v1/flightdata", "flightdata",
+        "/v1/flightdata/", "flightdata",
+        "/v1/flightdata/(.*)", "flightdata",
     )
 
+    #Create the web application and run it
     app = web.application(urls, globals())
     app.run()
--- weather.py
+++ weather.py
@@ -1,39 +1,39 @@
 import requests
 import json
 
-def getWeather(latitude = 35.8778, longitude = -78.7875, service = 'weather') :
-    '''getWeather: Gets the weather data from a web service. 
+def getWeather(latitude = 35.8778, longitude = -78.7875, service = "weather") :
+    """getWeather: Gets the weather data from a web service. 
     It requires the following parameters as inputs:
         latitude: The latitude of the place; -90 to +90 for south pole to north pole.
                     The default is for RDU.
         longitude: -180 to +180
         service: Can be any of "weather" or "forecast"
-    '''
-    keyid = '0d07fefdfd37c8a225916bf1ac394cc0'
-    url = 'http://api.openweathermap.org/data/2.5/'
+    """
+    keyid = "0d07fefdfd37c8a225916bf1ac394cc0"
+    url = "http://api.openweathermap.org/data/2.5/"
 
 
     url += service
 
     payload = {
-        'APPID':  keyid,
-        'lon': longitude,
-        'lat':  latitude,
-        'units': 'metric'
+        "APPID":  keyid,
+        "lon": longitude,
+        "lat":  latitude,
+        "units": "metric"
         }
-    headers = {'content-type': 'application/json'}
+    headers = {"content-type": "application/json"}
 
     r = requests.get(url, headers=headers, params=payload)
 
     fdata = r.json()
     data = {
-        'name': fdata.get('name'),
-        'country': fdata['sys']['country'],
-        'latitude': fdata['coord']['lat'],
-        'longitude': fdata['coord']['lon'],
-        'temp': fdata['main']['temp'],
-        'humidity': fdata['main']['humidity'],
-        'wind': fdata['wind'],
+        "name": fdata.get("name"),
+        "country": fdata["sys"]["country"],
+        "latitude": fdata["coord"]["lat"],
+        "longitude": fdata["coord"]["lon"],
+        "temp": fdata["main"]["temp"],
+        "humidity": fdata["main"]["humidity"],
+        "wind": fdata["wind"],
         }
     return data
 
